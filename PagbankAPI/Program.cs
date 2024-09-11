6aeda0d07b11aafe7efc546a9296489f5ee6b9a5
Tiago Pedro Jr <tpj.tiago@gmail.com>
2024-09-11 13:06:23 -0300

    First Commit

--- PagbankAPI/Program.cs
+++ PagbankAPI/Program.cs
@@ -11,11 +11,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
